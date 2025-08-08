22eb6dc0293607e6588a470b6a6187d9bff72364
Vladimir Zagray <vladimir.zagrai@coral.team>
2025-08-08 23:16:45 +0300

    feat(642): cherry-pick the fix
    Co-authored-by: hhowe29 <hhowe29@users.noreply.github.com>

--- src/dialects/dialects.ts
+++ src/dialects/dialects.ts
@@ -5,7 +5,10 @@ import { sqliteOptions } from "./sqlite";
 import { DialectOptions } from "./dialect-options";
 import { Dialect } from "sequelize";
 
-export const dialects: { [name in Dialect]: DialectOptions } = {
+export const dialects: { [name in Dialect]: DialectOptions | null } = {
+  db2: null,
+  oracle: null,
+  snowflake: null,
   mssql: mssqlOptions,
   mysql: mysqlOptions,
   mariadb: mysqlOptions,
